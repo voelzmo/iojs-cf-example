fa4700b34f0217d648dc0463029a0803e40e5b4c
Marco Voelz <marco.voelz@sap.com>
2015-07-31 16:26:04 +0200

    Print VCAP_SERVICES env variable

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ let appEnv = cfenv.getAppEnv()
 
 let server = http.createServer(function(request, resource) {
   resource.writeHead(200)
-  resource.end("Helloooooooo")
+  resource.write("Helloooooooo")
+  resource.write(`Contents of the 'service' environment: ${appEnv.getServices()}`)
 })
 
 let port = appEnv.isLocal? 8080 : appEnv.port
