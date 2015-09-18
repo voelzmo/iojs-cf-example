56d9673ba5816178c96dff1e8c30b43cbce4245c
Marco Voelz <marco.voelz@sap.com>
2015-09-18 17:24:04 +0200

    Add influxdb service

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ let http = require('http')
 let util = require('util')
 let cfenv = require('cfenv')
 let pg = require('pg')
-
+let graphite = require('graphite')
+let influx = require('influx')
 
 let appEnv = cfenv.getAppEnv()
 
@@ -25,19 +26,89 @@ function connectToPG(conString){
 	})
 }
 
+function connectoToInfluxDB(influxDBServiceCredentials) {
+	let host = influxDBServiceCredentials.hostname
+	let graphitePort = influxDBServiceCredentials.ports["2003/tcp"]
+  console.log(`Connecting to the graphite port at 'plaintext://${host}:${graphitePort}''`)
+
+	let client = graphite.createClient(`plaintext://${host}:${graphitePort}`)
+	let metricsExample = { }
+	metricsExample["foo.bar"] = 42
+
+	console.log(`writing example data: ${util.inspect(metricsExample)}`)
+	client.write(metricsExample, function(err){
+		console.log(`Some horrible error occurred while writing to graphite: ${util.inspect(err)}`)
+	})
+}
+
+function writeInfluxDBContentToStdout(influxDBServiceCredentials) {
+	let host = influxDBServiceCredentials.hostname
+	let port = influxDBServiceCredentials.ports["8086/tcp"]
+	let password = influxDBServiceCredentials.password
+	let username = influxDBServiceCredentials.username
+	let database = influxDBServiceCredentials.dbname
+	console.log(`Creating influxdb client with params host: ${host}, port: ${port}, password: ${password}, username: ${username}, database: ${database}`)
+	let client = influx({
+		host,
+		port,
+		username,
+		password,
+		database
+	})
+  let measurements = client.getMeasurements()
+	console.log(`measurements from influxdb: ${measurements}`)
+
+	let values = client.getSeries("foo.bar", function(err, arraySeriesNames){
+		if (err) {
+			console.console.log(`Error during gettings series: ${util.inspect(err)}`)
+		} else {
+			console.log(`Results of series query: ${util.inspect(arraySeriesNames, {depth: null})}`)
+		}
+	})
+
+
+	let point = { value : 23 }
+	client.writePoint("foo.bar", point, null, function(err, response){
+		if (err){
+				console.log(`Error during writing series: ${util.inspect(err)}`)
+		}
+		if (response){
+			console.log(`Response during writing series: ${util.inspect(response, {depth: null})}`)
+		}
+	})
+
+	values = client.getSeries("foo.bar", function(err, arraySeriesNames){
+		if (err) {
+			console.console.log(`Error during gettings series: ${util.inspect(err)}`)
+		} else {
+			console.log(`Results of series query: ${util.inspect(arraySeriesNames, {depth: null})}`)
+		}
+	})
+}
+
 let server = http.createServer(function(request, resource) {
   resource.writeHead(200)
-  resource.write("Helloooooooo\n")
-  resource.write(`Contents of the 'service' environment: ${util.inspect(appEnv.getServices())}`)
+  resource.write("Helloooooooo World now new\n")
+  resource.write(`Contents of the 'service' environment: ${util.inspect(appEnv.getServices(), {depth: null})}`)
+	resource.end()
 
-  let conString = appEnv.getServiceCreds("pg-svc").uri
-  if (conString) {
-    console.log("Connecting to postgres database...");
-  	connectToPG(conString)
+  let postgresCredentials = appEnv.getServiceCreds("pg-svc")
+  if (postgresCredentials) {
+		let postgresConnectionString = postgresCredentials.uri
+    console.log("Connecting to postgres database...")
+  	connectToPG(postgresConnectionString)
   } else {
-    console.log("No postgres service configured with the name 'pg-svc'");
+    console.log("No postgres service configured with the name 'pg-svc'")
   }
 
+	let influxDBServiceCredentials = appEnv.getServiceCreds('marco-influxdb-with-graphite')
+	if (influxDBServiceCredentials) {
+		console.log("Connecting to influxdb database...")
+		connectoToInfluxDB(influxDBServiceCredentials)
+    writeInfluxDBContentToStdout(influxDBServiceCredentials)
+  }	else {
+		console.log("No influxb service configured with the name 'marco-influxdb'")
+	}
 })
 
 let port = appEnv.isLocal? 8080 : appEnv.port
